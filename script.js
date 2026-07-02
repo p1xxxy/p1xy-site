25342bb3d004b9fa18e9db0c5f3dfdf7a2abf77f
P1XY <p1xy@local.dev>
2026-07-02 10:04:47 +0300

    Create canvas background foundation

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("P1XY website loaded");
+const canvas = document.getElementById("network");
+const ctx = canvas.getContext("2d");
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+resizeCanvas();
+
+window.addEventListener("resize", resizeCanvas);
+
+ctx.fillStyle = "#00b7ff";
+ctx.beginPath();
+ctx.arc(200, 200, 4, 0, Math.PI * 2);
+ctx.fill();
